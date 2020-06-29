dce42b0ce001b04e716a1da34652f6d51e4ca066
Lucia Rumbo <lucia@pigmalionstudios.com.ar>
2020-06-29 09:48:39 -0300

    example project with a simple /hello endpoint

---
+++ readme.md
@@ -0,0 +1,22 @@
+## Reactive Web Service Example
+
+From tutorial 
+
+https://spring.io/guides/gs/reactive-rest-service/[https://spring.io/guides/gs/reactive-rest-service
+
+Uses Webflux Spring 5 Framework for creates a simple endpoint /hello made with functional programming, and a Netty Web Server embedded.
+
+For run, execute:
+
+````
+mvn spring-boot:run
+````
+
+For create a JAR file
+````
+mvn clean package
+````
+````
+java -jar target/gs-reactive-rest-service-0.1.0.jar
+````
+
---
+++ src/main/java/hello/GreetingRouter.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+@Configuration
+public class GreetingRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route (GreetingHandler greetingHandler) {
+        return RouterFunctions
+                .route(
+                        RequestPredicates
+                                .GET("/hello")
+                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                        greetingHandler::hello
+                );
+    }
+
+}
